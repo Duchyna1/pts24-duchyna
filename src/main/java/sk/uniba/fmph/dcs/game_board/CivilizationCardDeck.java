f47e5b0db399ea7f7a372d6e27443795fb5b1717
Dusan Dusek <matus.duchyna@gmail.com>
2024-11-27 12:18:48 +0100

    implement CivilizationCardDeck

--- src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeck.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeck.java
@@ -1,15 +1,29 @@
 package sk.uniba.fmph.dcs.game_board;
 
-import sk.uniba.fmph.dcs.stone_age.CivilisationCard;
+import sk.uniba.fmph.dcs.stone_age.CivilizationCard;
 
 import java.util.Optional;
+import java.util.Stack;
 
 public final class CivilizationCardDeck {
-    public Optional<CivilisationCard> getTop() {
-        return Optional.empty();
+    private Stack<CivilizationCard> deck;
+
+    public CivilizationCardDeck(final CivilizationCard[] cards) {
+        this.deck = new Stack<>();
+        for (CivilizationCard card : cards) {
+            this.deck.push(card);
+        }
+    }
+
+    public Optional<CivilizationCard> getTop() {
+        if (this.deck.empty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(this.deck.pop());
     }
 
     public String state() {
-        return "TODO";
+        return "Deck size: " + this.deck.size();
     }
 }
